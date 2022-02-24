b816b2a5a6294d4b48b6e2c89cbb0b711545f386
Joachim <joachim.berner@hotmail.com>
2022-02-24 11:51:17 +0100

    Vi på begyndt vores login system, vi anvender en tutorial fra hjemmesiden CodeShack på linket https://codeshack.io/secure-login-system-php-mysql/

--- settings/init.php
+++ settings/init.php
@@ -1,18 +1,57 @@
 <?php
-require "classes/classDB.php";
+session_start();
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'phplogin';
 
-define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
-if(CONFIG_LIVE == 0){
-    $DB_SERVER = "localhost";
-    $DB_NAME = "test";
-    $DB_USER = "root";
-    $DB_PASS = "";
-}else{
-    $DB_SERVER = "";
-    $DB_NAME = "";
-    $DB_USER = "";
-    $DB_PASS = "";
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+    // If there is an error with the connection, stop the script and display the error.
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-$db = new db($DB_SERVER, $DB_NAME, $DB_USER, $DB_PASS);
+
+
+// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+if ( !isset($_POST['username'], $_POST['password']) ) {
+    // Could not get the data that should have been sent.
+    exit('Please fill both the username and password fields!');
+}
+
+
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+    // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+    $stmt->bind_param('s', $_POST['username']);
+    $stmt->execute();
+    // Store the result so we can check if the account exists in the database.
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        // Account exists, now we verify the password.
+        // Note: remember to use password_hash in your registration file to store the hashed passwords.
+        if (password_verify($_POST['password'], $password)) {
+            // Verification success! User has logged-in!
+            // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+            header('Location: https://www.codeproject.com/Questions/711838/Multiple-database-to-a-single-website');
+        } else {
+            // Incorrect password
+            echo 'Incorrect password!';
+        }
+    } else {
+        // Incorrect username
+        echo 'Incorrect username!';
+    }
+
+    $stmt->close();
+}
