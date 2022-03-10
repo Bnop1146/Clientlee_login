0fed348ced5336152556d6e408688dff3bd0971f
Joachim <joachim.berner@hotmail.com>
2022-03-10 10:26:12 +0100

    Sidste skrammel til vores Dashboard

--- home.php
+++ home.php
@@ -6,6 +6,9 @@ if (!isset($_SESSION['loggedin'])) {
     header('Location: index.html');
     exit;
 }
+//$hashed_password = password_hash('test', PASSWORD_DEFAULT);
+//echo $hashed_password;
+
 ?>
 
 
