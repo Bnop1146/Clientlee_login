c4b853f301a3f82094705319ae5204cc1fe859ee
Joachim <joachim.berner@hotmail.com>
2022-03-10 11:39:22 +0100

    Så Ømer kan arbejde på siden.

--- home.php
+++ home.php
@@ -70,18 +70,10 @@ if (!isset($_SESSION['loggedin'])) {
             <div class="col">
                 <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-1.jpg');">
                     <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                        <h2 class="pb-5 mt-5 mb-4 display-6 lh-1 fw-bold">Tryk her for hurtigt at komme til din kundeoversigt</h2>
+                        <h2 class="pb-5 mt-5 mb-4 display-6 lh-1 fw-bold">Tryk på pilen for hurtigt at komme til din kundeoversigt</h2>
                         <ul class="d-flex list-unstyled mt-auto">
                             <li class="me-auto">
-                                <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
-                            </li>
-                            <li class="d-flex align-items-center me-3">
-                                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
-                                <small>Earth</small>
-                            </li>
-                            <li class="d-flex align-items-center">
-                                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"/></svg>
-                                <small>3d</small>
+                                <a href="customers.php"><i class='bx bxs-chevrons-left bx-lg'></i></a>
                             </li>
                         </ul>
                     </div>
@@ -95,15 +87,7 @@ if (!isset($_SESSION['loggedin'])) {
                         <h2 class="pb-5 mt-5 mb-4 display-6 lh-1 fw-bold">Ønsker du at oprette nye kunder kan du trykke her</h2>
                         <ul class="d-flex list-unstyled mt-auto">
                             <li class="me-auto">
-                                <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
-                            </li>
-                            <li class="d-flex align-items-center me-3">
-                                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
-                                <small>California</small>
-                            </li>
-                            <li class="d-flex align-items-center">
-                                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"/></svg>
-                                <small>5d</small>
+                                <a href="customers.php"><i class='bx bxs-chevrons-left bx-lg'></i></a>
                             </li>
                         </ul>
                     </div>
