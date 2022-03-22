9b06596ff74a51c90720a5c3020d019df702a2a5
Joachim <joachim.berner@hotmail.com>
2022-03-22 15:10:26 +0100

    Glen har hjulpet os med at fikse vores PHP problemer med vores Edit side, og med at øge sikkerheden på vores side, ved at binde alle vores data, så vores SQL ikke er svag overfor sql-injection. Vi fik også en forklaring i hvad vores DB-class gør, så vi ikke connector til vores database flere gange ved at skrive den samme kode 2 gange.

--- navigation.php
+++ navigation.php
@@ -7,7 +7,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
