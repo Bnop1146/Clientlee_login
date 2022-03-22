9326b5e1163906b5b43d055628d6befeacbe8632
Joachim <joachim.berner@hotmail.com>
2022-03-22 15:10:15 +0100

    Glen har hjulpet os med at fikse vores PHP problemer med vores Edit side, og med at øge sikkerheden på vores side, ved at binde alle vores data, så vores SQL ikke er svag overfor sql-injection. Vi fik også en forklaring i hvad vores DB-class gør, så vi ikke connector til vores database flere gange ved at skrive den samme kode 2 gange.

--- modify.php
+++ modify.php
@@ -2,28 +2,30 @@
 
 require 'init.php';
 
-$id = $_POST['id'];
-$kundeDato = $_POST['kundeDato'];
-$kundeNavn = $_POST['kundeNavn'];
-$kundeTelefon = $_POST['kundeTelefon'];
-$kundeEmail = $_POST['kundeEmail'];
-$kundeAdresse = $_POST['kundeAdresse'];
-$kundePost = $_POST['kundePost'];
+$id = $_GET['kundeId'];
 
 $sql = "UPDATE customers SET 
-                     `kundeDato`= '$kundeDato',
-                     `kundeNavn`= '$kundeNavn',
-                     `kundeTelefon`= '$kundeTelefon',
-                     `kundeEmail`= '$kundeEmail',
-                     `kundeAdresse`= '$kundeAdresse',
-                     `kundePost`= '$kundePost' 
-        WHERE kundeId = $id ";
+                     kundeDato = :kundeDato,
+                     kundeNavn = :kundeNavn,
+                     kundeTelefon = :kundeTelefon,
+                     kundeEmail = :kundeEmail,
+                     kundeAdresse = :kundeAdresse,
+                     kundePost = :kundePost,
+                     kundeKommentar = :kundeKommentar
+        WHERE kundeId = :kundeId";
 
+$bind = [
+    ":kundeDato" => $_POST["kundeDato"],
+    ":kundeNavn" => $_POST["kundeNavn"],
+    ":kundeTelefon" => $_POST["kundeTelefon"],
+    ":kundeEmail" => $_POST["kundeEmail"],
+    ":kundeAdresse" => $_POST["kundeAdresse"],
+    ":kundePost" => $_POST["kundePost"],
+    ":kundeKommentar" => $_POST["kundeKommentar"],
+    ":kundeId" => $id,
 
-$result = $con->query($sql);
-$con->close();
+];
+
+$result = $db->sql($sql, $bind, false);
 
 header("Location: customers.php");
-
-
-
